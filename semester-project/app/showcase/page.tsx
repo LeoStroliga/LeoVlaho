321e53579402591198c0f03c1dc63f8029c04d50
unknown <leostroliga1@gmail.com>
2023-11-05 17:06:59 +0100

    Update after Vjezba 3

--- semester-project/app/showcase/page.tsx
+++ semester-project/app/showcase/page.tsx
@@ -1,9 +1,7 @@
-function Showcase() {
-    return (
-        <main className="flex justify-center text-4xl p-14">
-        <h1>Showcase Page</h1>
-      </main>  
-    );
+export default function Showcase() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
+      <h1 className="text-3xl font-bold">Showcase Page</h1>
+    </main>
+  );
 }
-
-export default Showcase;
