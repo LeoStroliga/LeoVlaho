1838b1b52c25777247ee562bb483a1f1b4762d78
Vlaho123 <vpetko00@fesb.hr>
2024-08-31 16:59:36 +0200

    Valid

---
+++ semester-project/app/UserResources/Title.tsx
@@ -0,0 +1,8 @@
+// RecipeTitle.js
+import React from 'react';
+
+const RecipeTitle = () => (
+    <h1 className="font-roboto-condensed text-6xl font-extrabold" style={{ color: '#2F855A', marginTop: '4rem' }}>Recipes</h1>
+);
+
+export default RecipeTitle;
